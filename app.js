a13fd2900240f0106dbb450b5d333722bc9e650a
Agnieszka <agusia2721@gmail.com>
2020-03-13 18:28:51 +0100

    menu item

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// stworzylam pliki w routes i odpowoiednie w views dla kadego Route
+// importuje dane routery do pliku app.js poprzez np. var newRouter = require('./routes/news');
+// następnie dodaje app.use('/', indexRouter); do kazej Route
+// aby nie powielac menu robimy menu w jednym miejscu -> tutaj w layout.pug
+
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,7 +11,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var newsRouter = require('./routes/news');
+var quizRouter = require('./routes/quiz');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -15,20 +23,31 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.locals.path = req.path;
+
+  next();
+})
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/news', newsRouter);
+app.use('/quiz', quizRouter);
+app.use('/admin', adminRouter);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
